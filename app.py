2cf26e7e917bd46a45e42da366f09e21f281b1d5
rmrusev24a <rmrusev24a@gmail.com>
2026-02-09 12:21:03 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,35 @@
+import streamlit as st
+
+st.title("Тест по математика")
+st.write("Отговори на въпросите и натисни **Изпрати**")
+
+q1 = st.number_input("1️⃣ Колко е 5 × 5 ?", step=1)
+q2 = st.number_input("2️⃣ Колко е 10 + 2 ?", step=1)
+q3 = st.number_input("3️⃣ Колко е 8 − 3 ?", step=1)
+q4 = st.number_input("4️⃣ Колко е 4 × 2 ?", step=1)
+q5 = st.number_input("5️⃣ Колко е 20 ÷ 4 ?", step=1)
+
+if st.button("Изпрати"):
+    score = 0
+
+    if q1 == 25:
+        score += 1
+    if q2 == 12:
+        score += 1
+    if q3 == 5:
+        score += 1
+    if q4 == 8:
+        score += 1
+    if q5 == 5:
+        score += 1
+
+    st.write("###Резултат:")
+    st.write(f"Верни отговори: **{score} / 5**")
+
+    if score == 5:
+        st.success("Отлично! Всичко е вярно!")
+    elif score >= 3:
+        st.warning("Добре, но можеш и по-добре!")
+    else:
+        st.error("Опитай отново!")
 
